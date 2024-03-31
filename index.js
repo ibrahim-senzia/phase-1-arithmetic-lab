696c8a193a62cd86942176bc5ab304388c03f0c8
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-31 09:00:41 +0300

    arithmetic-lab completed

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+
+//an equation whose multiplied results = 62
+let num1 = 1
+let num2 = 62
+let multiply = num1 * num2;
+// => 62
+
+//Create a variable called random that will generate a random integer greater than 0.
+let random = Math.floor(Math.random () * 10);
+
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
+let max = Math.max(17,5,10,19,20);
